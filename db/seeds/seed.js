d6899308a43c5216cfce4e3137d6423dcbe1a203
flor1n-flo <florinpatroescu@gmail.com>
2025-05-31 06:21:38 +0100

    Project setup, seeding-creating tables, and insert data

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,98 @@
 const db = require("../connection")
+const format = require("pg-format");
+const {
+  convertTimestampToDate,
+  createArticleRef,
+  formatComments,
+} = require ("./utils")
 
-const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+const seed = async ({ topicData, userData, articleData, commentData }) => {
+  //return db.query();//<< write your first query in here.
+  await db.query(`DROP TABLE IF EXISTS comments, articles, users, topics CASCADE;`);
+
+  //creating tables
+
+  await db.query(`
+    CREATE TABLE topics (
+      slug VARCHAR PRIMARY KEY,
+      description VARCHAR NOT NULL,
+      img_url VARCHAR(1000)
+      );
+    `);
+
+  await db.query(`
+    CREATE TABLE users (
+     username VARCHAR PRIMARY KEY,
+     name VARCHAR NOT NULL,
+     avatar_url VARCHAR(1000)
+    );
+  `);
+
+  await db.query(`
+    CREATE TABLE articles (
+      article_id SERIAL PRIMARY KEY,
+      title VARCHAR NOT NULL,
+      topic VARCHAR REFERENCES topics(slug) NOT NULL,
+      author VARCHAR REFERENCES users(username) NOT NULL,
+      body TEXT NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      votes INT DEFAULT 0,
+      article_img_url VARCHAR(1000)
+    );
+  `);
+
+  await db.query (`
+    CREATE TABLE comments (
+      comment_id SERIAL PRIMARY KEY,
+      author VARCHAR REFERENCES users(username) NOT NULL,
+      article_id INT REFERENCES articles(article_id) ON DELETE CASCADE,
+      votes INT DEFAULT 0,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      body TEXT NOT NULL
+    );
+  `);
+
+
+  //Insert in topics
+  const topicInsertQueryStr = format(
+    `INSERT INTO topics (slug, description, img_url) VALUES %L RETURNING *;`,
+    topicData.map(({ slug, description, img_url }) => [slug, description, img_url])
+  );
+  await db.query(topicInsertQueryStr);
+  
+
+  //Insert in users
+  const userInsertQueryStr = format(
+    `INSERT INTO users (username, name, avatar_url) VALUES %L RETURNING *;`,
+    userData.map(({ username, name, avatar_url }) => [username, name, avatar_url])
+  );
+  await db.query(userInsertQueryStr);
+
+
+  //convert the articel timestamp
+  const formattedArticles = articleData.map(convertTimestampToDate);
+
+  //Insert in articles
+  const articleInsertQueryStr = format(
+    `INSERT INTO articles(title, topic, author, body, created_at, votes, article_img_url) VALUES %L RETURNING *;`,
+    formattedArticles.map(({title, topic, author, body, created_at, votes, article_img_url}) => 
+      [title, topic, author, body, created_at, votes, article_img_url])
+  );
+  const insertedArticles = await db.query(articleInsertQueryStr);
+
+  //create article reference map
+  const articleRef = createArticleRef(insertedArticles.rows);
+
+  
+  //format and insert comments 
+  const formattedComments = formatComments(commentData, articleRef);
+
+  //isnert in comments
+  const commentInsertQueryStr = format(
+    `INSERT INTO comments (author, article_id, votes, created_at, body) VALUES %L RETURNING *;`,
+    formattedComments.map(({ author, article_id, votes, created_at, body }) => [author, article_id, votes, created_at, body])
+  );
+  await db.query(commentInsertQueryStr);
+
 };
 module.exports = seed;
--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -5,5 +5,24 @@ exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
   return { created_at: new Date(created_at), ...otherProperties };
 };
 
+exports.createArticleRef = (articleRows) => {
+  const articleRef = {};
+  articleRows.forEach(({ title, article_id }) => {
+    articleRef[title] = article_id;
+  });
+  return articleRef;
+};
+
+exports.formatComments = (comments, articleRef) => {
+  return comments.map(({ created_by, belongs_to, created_at, ...rest }) => {
+    const formattedComment = {
+      author: created_by,
+      article_id: articleRef[belongs_to],
+      created_at: new Date(created_at),
+      ...rest,
+    };
+    return formattedComment;
+  });
+};
 
 
