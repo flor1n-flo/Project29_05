f8efeb35ce0a8cfb37aa627670dbaf8bda3a4626
flor1n-flo <florinpatroescu@gmail.com>
2025-06-11 07:04:25 +0100

    Seting up database on supabase

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,27 @@
 const { Pool } = require("pg");
+const path = require("path");
 
 const ENV = process.env.NODE_ENV || 'development'
 
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
-
-const db = new Pool();
-
-const config = {};
-
-if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
-
+//require("dotenv").config({path: path.join(__dirname, `../.env.${ENV}`),});
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
 }
 
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        max: 2,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
 
-module.exports = new Pool(config);
-module.exports = db;
+console.log("Connecting to:", ENV === "production" ? process.env.DATABASE_URL : process.env.PGDATABASE);
+
+
+const pool = new Pool(config);
+module.exports = pool;
