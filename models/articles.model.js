0c4d9f50e3099d99f81d35b5a4dff49a2f993b63
flor1n-flo <florinpatroescu@gmail.com>
2025-06-27 11:45:32 +0100

    fix typo in arrticle not found message

--- models/articles.model.js
+++ models/articles.model.js
@@ -86,8 +86,8 @@ exports.updateArticleVotes = (article_id, inc_votes) => {
 exports.checkIfArticleExists = (article_id) => {
   return db
     .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
-    .then((result) => {
-      if (result.rows.length === 0 ){
+    .then(({ rows }) => {
+      if (rows.length === 0 ){
         return Promise.reject({ status: 404, msg: "Not found" });
       }
     });
